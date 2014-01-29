2470ba1eff91be0d5aaddd04967f5d11983ff8cd
Gene Boggs <gene@cpan.org>
2014-01-29 03:48:51 +0000

    Import of GENE/Game-Theory-TwoPersonMatrix-0.01 from CPAN.
    gitpan-cpan-distribution: Game-Theory-TwoPersonMatrix
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         GENE/Game-Theory-TwoPersonMatrix-0.01.tar.gz
    gitpan-cpan-author:       GENE
    gitpan-cpan-maturity:     released

---
+++ README.md
@@ -0,0 +1,6 @@
+Game::Theory::TwoPersonMatrix
+=============================
+
+Perl API for two person matrix game reduction by elimination of strictly dominated strategies.
+
+Nash equilibria are also identified.
---
+++ lib/Game/Theory/TwoPersonMatrix.pm
@@ -0,0 +1,247 @@
+package Game::Theory::TwoPersonMatrix;
+BEGIN {
+  $Game::Theory::TwoPersonMatrix::AUTHORITY = 'cpan:GENE';
+}
+
+# ABSTRACT: Reduce & analyze a 2 person matrix game
+
+our $VERSION = '0.01';
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use Algorithm::Combinatorics qw( variations_with_repetition );
+use List::Util qw( max );
+use List::MoreUtils qw( all indexes each_array );
+
+
+
+sub new {
+    my $class = shift;
+    my %args = @_;
+    my $self = {
+        1 => $args{1} || { 1 => [1,0], 2 => [0,1] },
+        2 => $args{2} || { 1 => [1,0], 2 => [0,1] }
+    };
+    bless $self, $class;
+    return $self;
+}
+
+
+sub reduce {
+    my $self = shift;
+
+    # Set the players.
+    my ($player, $opponent) = @_;
+    ($player, $opponent) = ($self->{$player}, $self->{$opponent});
+
+    # Declare the bucket of "X given Y" strategy pair utilities.
+    my $utility = {};
+    my $metric  = {};
+
+    # Evaluate pairs of strategies.
+    my $iter = variations_with_repetition([keys %$player], 2);
+    while (my $v = $iter->next) {
+        # Skip "X|X" pairs.
+        next if $v->[0] eq $v->[1];
+
+        # Inspect each stategy utility.
+        #warn join(', ', @$v), "\n";
+        for my $i (0 .. @$v - 1) {
+            # Only consider defined utilities.
+            if (defined $player->{$v->[0]}[$i] && defined $player->{$v->[1]}[$i]) {
+                #warn "$i: $player->{$v->[0]}[$i] vs $player->{$v->[1]}[$i]\n";
+
+                # Add a relative utility indicator.
+                if ($player->{$v->[0]}[$i] > $player->{$v->[1]}[$i]) {
+                    # Strictly dominant utility
+                    push @{ $utility->{join '|', @$v} }, 1;
+                }
+                elsif ($player->{$v->[0]}[$i] < $player->{$v->[1]}[$i]) {
+                    # Strictly dominated utility
+                    push @{ $utility->{join '|', @$v} }, -1;
+                }
+                else {
+                    # Equivalent utility
+                    push @{ $utility->{join '|', @$v} }, 0;
+                }
+
+                # Track the actual relative utility metric.
+                #push @{ $metric->{join '|', @$v} }, $player->{$v->[0]}[$i] - $player->{$v->[1]}[$i];
+            }
+        }
+    }
+
+    # Remove a strictly dominated strategy.
+    for my $strat (keys %$utility) {
+        if (all { $_ == -1 } @{$utility->{$strat}}) {
+            #warn "U:[@{$utility->{$strat}}]\n";
+
+            # Capture the strategies.
+            my @dominated = split /\|/, $strat;
+            #warn "D:'$dominated[0]'\n";
+
+            # Remove the S.D. strategy.
+            delete $player->{$dominated[0]};
+
+            # Remove the utilities for the deleted opponent strategy.
+            for my $u (keys %$opponent) {
+                splice @{$opponent->{$u}}, $dominated[0] - 1, 1;
+                #warn "u:'@{$opponent->{$u}}'\n";
+            }
+
+            # Currently, we only reduce by a single strategy.
+            last;
+        } 
+    }
+
+    return $player, $opponent;
+}
+
+
+sub nash {
+    my $self = shift;
+
+    # Convenience:
+    my ($player, $opponent) = ($self->{1}, $self->{2});
+
+    # Inspect each player item for best strategy.
+    my %x; # Max utility indexes.
+
+    # Get the best strategies of the player.
+    for my $u ( sort { $a <=> $b } keys %$player ) {
+        # Find the maximum utility for the strategy.
+        my $max = max @{ $player->{$u} };
+
+        # Get the "strategically most desirable" indexes.
+        $x{$u} = [ indexes { $_ >= $max } @{ $player->{$u} } ];
+    }
+
+    # Inspect each opponent item for best strategy...
+    my %y;
+    for my $u ( sort { $a <=> $b } keys %$opponent ) {
+        my $max = max @{ $opponent->{$u} };
+        $y{$u} = [ indexes { $_ >= $max } @{ $opponent->{$u} } ];
+    }
+    #warn 'X:',Data::Dumper->new([\%x])->Indent(1)->Terse(1)->Quotekeys(0)->Sortkeys(1)->Dump;
+    #warn 'Y:',Data::Dumper->new([\%y])->Indent(1)->Terse(1)->Quotekeys(0)->Sortkeys(1)->Dump;
+
+    # Identify the Nash equilibria.
+    my $nash = {};
+
+    # Inspect index pairs of strategies.
+    my @xstrat = sort { $a <=> $b } keys %x;
+    my @ystrat = sort { $a <=> $b } keys %y;
+    my $estrat = each_array(@xstrat, @ystrat);
+    while ( my ($xs, $ys) = $estrat->() ) {
+        #warn "xs:'@{$x{$xs}}'\n";
+        #warn "ys:'@{$y{$ys}}'\n";
+
+        # Inspect index pairs of utilities.
+        my $eutil = each_array(@{$x{$xs}}, @{$y{$ys}});
+        while ( my ( $i, $j ) = $eutil->() ) {
+            # Are the best strategies for both players on the same coordinate?
+            if ( $i == $j ) {
+                #warn "$xs,$ys: $i == $j\n";
+
+                # Save the strategy coordinate and utilities.
+                $nash->{"$xs,$ys"} = [ $player->{$xs}[$i], $opponent->{$ys}->[$j] ];
+            }
+        }
+    }
+
+    return $nash;
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Game::Theory::TwoPersonMatrix - Reduce & analyze a 2 person matrix game
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+  use Game::Theory::TwoPersonMatrix;
+  my $g = Game::Theory::TwoPersonMatrix->new(
+    1 => { 1 => \@u1, 2 => \@u2 },
+    2 => { 1 => \@v1, 2 => \@v2 }
+  );
+  $g->reduce(2, 1);
+  $g->reduce(1, 2);
+  my $e = $g->nash;
+  print Dumper $e;
+
+=head1 DESCRIPTION
+
+A C<Game::Theory::TwoPersonMatrix> reduces and analyzes a two person matrix game
+of player names, strategies and numerical utilities.
+
+The players must have the same number of strategies, and each strategy must have
+the same size utility vectors as all the others.
+
+=head1 NAME
+
+Game::Theory::TwoPersonMatrix - Reduce & analyze a 2 person matrix game
+
+=head1 METHODS
+
+=head2 new()
+
+  my $g = Game::Theory::TwoPersonMatrix->new(%args);
+
+Create a new C<Game::Theory::TwoPersonMatrix> object.
+
+Player defaults:
+
+  1 => { 1 => [1,0], 2 => [0,1] },
+  2 => { 1 => [1,0], 2 => [0,1] }
+
+Player strategies are given by a 2D matrix of utilities (or payoffs) such that,
+
+  [ [ u1, u2 .. un] .. [ v1, v2 .. vn ] ]
+
+Where each "B<u>i" is a utility or payoff for the strategy "B<U>."
+
+=head2 reduce()
+
+  $g->reduce_game(1, 2); # Player 1 given opponent 2
+  print Dumper $g->{1}, $g->{2};
+  $g->reduce_game(2, 1); # Player 2 given opponent 1
+  print Dumper $g->{1}, $g->{2};
+
+Reduce the game by elimination of a single strictly dominated strategy of the
+player.
+
+Use repeated application of this method to solve a game, or verify that it is
+insoluble.
+
+=head2 nash()
+
+  my $equilibria = $g->nash;
+  print Dumper $equilibria;
+
+Find the Nash equilibria.
+
+=head1 AUTHOR
+
+Gene Boggs <gene@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Gene Boggs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
